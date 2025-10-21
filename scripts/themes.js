991fdb939b485ded92872774b7df7a8082813976
Leander List <list@ocg.at>
2025-10-21 12:17:38 +0200

    add basic theme manager

---
+++ scripts/themes.js
@@ -0,0 +1,60 @@
+async function loadTheme(themeName) {
+    try {
+        const response = await fetch(`../style/themes/${themeName}.json`);
+        if (!response.ok) throw new Error("Theme konnte nicht geladen werden");
+        const theme = await response.json();
+
+        //Background Styling
+        htmlClassList = document.documentElement.classList;
+        if (theme.bg.type === "color") {
+            htmlClassList.add(`bg-[${theme.bg.value}]`);
+        }
+        htmlClassList.add(`text-[${theme.bg.text}]`);
+        for (const element of theme.bg.classes.split(" ")) {
+            if(element.trim() === "") continue;
+            htmlClassList.add(element);
+        }
+
+        //Header Styling
+        const header = document.querySelector('header');
+        headerClassList = header.classList;
+        if (theme.header.type === "color") {
+            headerClassList.add(`bg-[${theme.header.value}]`);
+        }
+        for (const element of theme.header.classes.split(" ")) {
+            if (element.trim() === "") continue;
+            headerClassList.add(element);
+        }
+
+        //Card Styling
+        const cards = document.querySelectorAll('.card');
+        cards.forEach((card) => {
+            cardClassList = card.classList;
+            if (theme.cards.type === "color") {
+                cardClassList.add(`bg-[${theme.cards.value}]`);
+            }
+            for (const element of theme.cards.classes.split(" ")) {
+                if (element.trim() === "") continue;
+                cardClassList.add(element);
+            }
+        });
+
+        //Billy Card Styling
+        const billyCard = document.getElementById('billy-card');
+        billyCardClassList = billyCard.classList;
+        for (const element of theme.cards.billy.classes.split(" ")) {
+            if (element.trim() === "") continue;
+            billyCardClassList.add(element);
+        }
+
+        //Tally Card Styling
+        const tallyCard = document.getElementById('tally-card');
+        tallyCardClassList = tallyCard.classList;
+        for (const element of theme.cards.tally.classes.split(" ")) {
+            if (element.trim() === "") continue;
+            tallyCardClassList.add(element);
+        }
+    } catch (error) {
+        console.error("Fehler beim Laden des Themas:", error);
+    }
+}
